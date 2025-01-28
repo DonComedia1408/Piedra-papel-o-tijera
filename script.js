7da3cad023e6c60c2ced971dfdd34ac63e3c68ef
Alex Radoi <123100117+DonComedia1408@users.noreply.github.com>
2025-01-28 10:28:22 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,81 @@
+function getRandomComputerResult() {
+    const options = ["Piedra", "Papel", "Tijeras"];
+    const randomIndex = Math.floor(Math.random() * options.length);
+    return options[randomIndex];
+  }
+  
+  function hasPlayerWonTheRound(player, computer) {
+    return (
+      (player === "Piedra" && computer === "Tijeras") ||
+      (player === "Tijeras" && computer === "Papel") ||
+      (player === "Papel" && computer === "Piedra")
+    );
+  }
+  
+  let playerScore = 0;
+  let computerScore = 0;
+  
+  function getRoundResults(userOption) {
+    const computerResult = getRandomComputerResult();
+  
+    if (hasPlayerWonTheRound(userOption, computerResult)) {
+      playerScore++;
+      return `¡Jugador gana! ${userOption} gana a ${computerResult}`;
+    } else if (computerResult === userOption) {
+      return `¡Es un empate! Ambos eligieron ${userOption}`;
+    } else {
+      computerScore++;
+      return `¡IA gana! ${computerResult} gana a ${userOption}`;
+    }
+  }
+  
+  const playerScoreSpanElement = document.getElementById("player-score");
+  const computerScoreSpanElement = document.getElementById("computer-score");
+  const roundResultsMsg = document.getElementById("results-msg");
+  const winnerMsgElement = document.getElementById("winner-msg");
+  const optionsContainer = document.querySelector(".options-container");
+  const resetGameBtn = document.getElementById("reset-game-btn");
+  
+  function showResults(userOption) {
+    roundResultsMsg.innerText = getRoundResults(userOption);
+    computerScoreSpanElement.innerText = computerScore;
+    playerScoreSpanElement.innerText = playerScore;
+  
+    if (playerScore === 3 || computerScore === 3) {
+      winnerMsgElement.innerText = `¡${
+        playerScore === 3 ? "Jugador" : "IA"
+      } gana el juego!`;
+  
+      resetGameBtn.style.display = "block";
+      optionsContainer.style.display = "none";
+    }
+  
+  };
+  function resetGame() {
+    playerScore = 0;
+    computerScore = 0;
+    playerScoreSpanElement.innerText = "0";
+    computerScoreSpanElement.innerText = "0";
+    resetGameBtn.style.display = "none";
+    optionsContainer.style.display = "block";
+    roundResultsMsg.innerText = "";
+    winnerMsgElement.innerText = "";
+  };
+  
+  resetGameBtn.addEventListener("click", resetGame);
+  
+  const rockBtn = document.getElementById("rock-btn");
+  const paperBtn = document.getElementById("paper-btn");
+  const scissorsBtn = document.getElementById("scissors-btn");
+  
+  rockBtn.addEventListener("click", function () {
+    showResults("Piedra");
+  });
+  
+  paperBtn.addEventListener("click", function () {
+    showResults("Papel");
+  });
+  
+  scissorsBtn.addEventListener("click", function () {
+    showResults("Tijeras");
+  });
